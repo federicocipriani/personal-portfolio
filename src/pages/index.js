414d4f9cf44ae322f98a6a3b5e155ed94da15def
Federico <64685113+federicocipriani@users.noreply.github.com>
2020-07-06 22:22:11 +0100

    first-commit-060720

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const HomePage = () => {
+  return (
+    <Layout>
+      <h1>Hello.</h1>
+      <h2>I am Federico, a front-end developer living in the UK.</h2>
+      <p>
+        Need a developer? <Link to="/contact">Contact me.</Link>
+      </p>
+    </Layout>
+  )
 }
+
+export default HomePage
