dd973d7fe8118d14e0415cee6e86e5d7ff26ce7f
Federico <64685113+federicocipriani@users.noreply.github.com>
2020-07-14 23:36:45 +0100

    changes-140720

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,6 +6,9 @@ const Hero = () => {
         <div className={heroStyles.hero}>
             <h1>Hello.</h1>
             <h2>I am Federico, a front-end developer living in the UK.</h2>
+            <br />
+            <br />
+            <h3>I love data, coding and design.</h3>
         </div>
     );
 };
