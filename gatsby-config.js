49e971b2a7edc8745521d35be64dc8b85d6ba3a4
Federico <64685113+federicocipriani@users.noreply.github.com>
2020-08-03 18:33:03 +0200

    google-analytics-integration-030820

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
         'gatsby-plugin-smoothscroll',
         'gatsby-plugin-react-helmet',
         {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+                trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+            },
+        },
+        {
             resolve: 'gatsby-source-contentful',
             options: {
                 spaceId: process.env.CONTENTFUL_SPACE_ID,
